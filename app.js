b1b4d0b898f2be7a4fbd6c679e73852c0d8ce7bd
Guilherme G Silva <gui_g88@hotmail.com>
2021-11-08 09:16:57 -0300

    third commit

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const storeBtn = document.getElementById("store-btn")
 const retrBtn = document.getElementById("retrieve-btn")
 
+const dbRequest = indexedDB.open("StorageDummy", 1)
+
+let db 
+
+dbRequest.onsuccess = e => {
+    db = e.target.result
+}
+
+dbRequest.onupgradeneeded = e => {
+    db = e.target.result
+
+    const objStore = db.createObjectStore("products", {keyPath: "id"})
+
+    objStore.transaction.oncomplete = e => {
+        const productsStore = db.transaction("products", "readwrite").objectStore("products")
+
+        productsStore.add({id: "p1", title: "A First Product", price: 12.99, tags: ["Expensive", "Luxury"]})
+    }
+}
+
+dbRequest.onerror = e => console.log("ERROR!")
+
 storeBtn.addEventListener("click", () => {
-    const userId = "u123"
-    document.cookie = `uid=${userId}`
+    if(!db) {
+        return
+    }
+
+    const productsStore = db.transaction("products", "readwrite").objectStore("products")
+
+    productsStore.add({id: "p2", title: "A Second Product", price: 122.99, tags: ["Expensive", "Luxury"]})
 })
 
 retrBtn.addEventListener("click", () => {
-    console.log(document.cookie)
+    const productsStore = db.transaction("products", "readwrite").objectStore("products")
+
+    const request = productsStore.get("p2")
+
+    request.onsuccess = () => console.log(request.result)
 })
 
